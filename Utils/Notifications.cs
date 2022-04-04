6256f2696ea0c49e23ca2187e5e9400992a8c126
Raul Luis <soyrauluis@gmail.com>
2022-04-03 22:26:16 -0400

    Create NotificationService Utils

---
+++ Utils/Notifications.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace ProyectoFinal.Utils;
+
+public class Notifications
+{
+    public static NotificationMessage Info(string message)
+    {
+        return new NotificationMessage()
+            {Severity = NotificationSeverity.Info, Summary = "Info", Detail = message, Duration = 4000};
+    }
+
+    public static NotificationMessage Success(string message)
+    {
+        return new NotificationMessage()
+            {Severity = NotificationSeverity.Success, Summary = "Success", Detail = message, Duration = 4000};
+    }
+
+    public static NotificationMessage Error(string message)
+    {
+        return new NotificationMessage()
+            {Severity = NotificationSeverity.Error, Summary = "Error", Detail = message, Duration = 4000};
+    }
+}
