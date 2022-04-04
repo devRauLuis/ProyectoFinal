e072b24d2e4b34e01449ba481e5a6b437ec90f0d
Raul Luis <soyrauluis@gmail.com>
2022-04-03 22:21:25 -0400

    Clean boilerplate

---
+++ DAL/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProyectoFinal.Entidades;
+
+namespace ProyectoFinal.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public DbSet<Paciente> Pacientes { get; set; }
+
+    public DbSet<Aseguradora> Aseguradoras { get; set; }
+
+    public DbSet<Sintoma> Sintomas { get; set; }
+
+    public DbSet<Cita> Citas { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Sintoma>().HasData(new Sintoma(){});
+    }
+    
+    
+}
